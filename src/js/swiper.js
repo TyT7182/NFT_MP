6068656690b4ea85e8fe6045e4751f611fea9ad2
Evgeniy <evgenvlad46@gmail.com>
2022-09-09 23:20:28 +0300

    two

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -22,6 +22,41 @@ const swiper = new Swiper('.swiper', {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
    },
+   breakpoints: {
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 32,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+      modules: [Navigation, Pagination],
+      navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+    },
+    1366: {
+      slidesPerView: 5,
+      spaceBetween: 32,
+      preventClicksPropagation: 'true',
+      effect: 'flip',
+      loop: true,
+      grabCursor: true,
+      modules: [Navigation, Pagination],
+      navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+    }
+
+
+   }
   
 
 });
@@ -34,30 +69,52 @@ const swiper = new Swiper('.swiper', {
 
 
 
-const mediaQuery = window.matchMedia('(max-width: 375px)')
-function handleTabletChange(e) {
-  if (e.matches) {
+// const mediaQuery = window.matchMedia('(min-width: 320px)')
+// function handleTabletChange(e) {
+//   if (e.matches) {
 
-    const swiper = new Swiper('.swiper', {
+//     const swiper = new Swiper('.swiper', {
 
 
 
-      slidesPerView: 1,
-      spaceBetween: 8,
-      allowTouchMove: 'false',
-      preventClicksPropagation: 'true',
-      simulateTouch: 'true',
-      effect: 'flip',
+//       slidesPerView: 1,
+//       spaceBetween: 32,
+
 
 
 
 
       
 
-    });
-  }
-}
-mediaQuery.addListener(handleTabletChange)
-handleTabletChange(mediaQuery)
+//     });
+//   }
+// }
+// mediaQuery.addListener(handleTabletChange)
+// handleTabletChange(mediaQuery)
 
 
+// const mediaQuery_2 = window.matchMedia('(max-width: 850px)')
+// function handleTabletChange_2(e) {
+//   if (e.matches) {
+
+//     const swiper = new Swiper('.swiper', {
+
+
+
+//       slidesPerView: 3,
+//       spaceBetween: 15,
+//       allowTouchMove: 'false',
+//       preventClicksPropagation: 'true',
+//       simulateTouch: 'true',
+//       effect: 'flip',
+
+
+
+
+      
+
+//     });
+//   }
+// }
+// mediaQuery_2.addListener(handleTabletChange_2)
+// handleTabletChange_2(mediaQuery_2)
